48f4a5d47a2d318f0444d21e298140fb2fb2ea5d
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-09-13 11:29:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import Header from './components/Cabecalho'
-import Hero from './components/Hero'
-import ListaVagas from './containers/ListaVagas'
+import Header from './components/Cabecalho';
+import Hero from './components/Hero';
+import ListaVagas from './containers/ListaVagas';
+import { GlobalStyle } from './global';
+import styled from 'styled-components';
 
-import './global.css'
+const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media (max-width: 1024px) {
+    max-width: 80%;
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
