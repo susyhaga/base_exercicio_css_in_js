aca17fc24c19b153de6b998addd468b0cdad7e7d
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-09-13 12:52:43 -0300

    Delete src directory

--- src/components/Cabecalho/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from './styles'
-
-const Cabecalho = () => (
-  <Header>
-    <h1>EBAC Jobs</h1>
-  </Header>
-);
-
-export default Cabecalho;
--- src/components/Cabecalho/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Header = styled.header`
-  background-color: var(--cor-secundaria);
-  color: var(--cor-principal);
-  text-align: center;
-  padding: 24px 0;
-`;
